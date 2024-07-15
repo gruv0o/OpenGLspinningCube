cfde630ca18204d5462fefd0004cb67cef728128
gruv0o <renaultremi51100@gmail.com>
2024-07-15 10:10:47 +0200

    v0.01 + OpenGL Init + Creating a Window + The Window stay open

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glewExperimental = true;
+    if(!glfwInit()){
+        fprintf( stderr, "Failed to init GLFW\n");
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1520, 720, "window", NULL,NULL);
+    if( window == NULL ){
+        fprintf(stderr,"Failed to open GLFW window.");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    
+
+    glewExperimental=true;
+    if (glewInit() != GLEW_OK){
+        fprintf(stderr, "Failed to init GLEW\n");
+        return -1;
+    }
+
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do{
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    while(glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
     return 0;
 }
